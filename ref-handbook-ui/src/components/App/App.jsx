9f2b5f291b25866864a6519a8c9107ad8cb909b0
travisnava <trn5106@psu.edu>
2022-07-13 12:51:18 -0400

    Added skeleton components for landing, navbar, register/login, also changed name of website in index and added constants.js

---
+++ ref-handbook-ui/src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+import './App.css'
+
+//react imports
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+export default function App() {
+
+  return (
+  <div className="app">
+    Hello World
+  </div>
+  )
+}
+
+
